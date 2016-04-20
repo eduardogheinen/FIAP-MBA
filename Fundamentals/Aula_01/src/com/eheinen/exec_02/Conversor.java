9f12f43a9e5ef1ced516ed738c715b85d516f43c
Eduardo Gomes Heinen <RM30563@fiap.com.br>
2016-04-20 19:52:06 -0300

    Initial Commit

---
+++ Fundamentals/Aula_01/src/com/eheinen/exec_02/Conversor.java
@@ -0,0 +1,14 @@
+package com.eheinen.exec_02;
+
+public class Conversor {
+	public static void main(String[] args){
+		
+	}
+	
+	private static String ConvertDecimalToBinary(int decimal){
+		int result = decimal / 2;
+		int mod = decimal % 2;
+				
+		return null;
+	}
+}
