97a74e972b88115a0d9ba1057b85e23ab3c2bb3d
Eduardo Gomes Heinen <RM30563@fiap.com.br>
2016-04-25 22:54:23 -0300

    - Exercise 4 was implemented successfully!

---
+++ Workspace/Fundamentals/Class_02/src/com/eheinen/exec_04/SavingAccount.java
@@ -0,0 +1,8 @@
+package com.eheinen.exec_04;
+
+public class SavingAccount extends BankAccount{
+
+	public SavingAccount(String clientName, String clientAddress, String clientCPF) {
+		super(clientName, clientAddress, clientCPF);
+	}
+}
